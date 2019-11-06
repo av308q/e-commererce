9f331ddc983ff8efb2fde0300aa5dbaf82c414ec
Angel Valdivia <angelv1901@gmail.com>
2019-11-06 08:52:06 -0500

    updated directory reducer

--- src/redux/directory/directory-reducer.js
+++ src/redux/directory/directory-reducer.js
@@ -9,33 +9,33 @@ const INITIAL_STATE = {
 			imageUrl: thumb,
 			size: "small",
 			id: 1,
-			linkUrl: "shop"
+			linkUrl: "shop/hats"
 		},
 		{
 			title: "jackets",
 			imageUrl: "https://i.ibb.co/3N7kr6n/new-paul.png",
 			id: 2,
-			linkUrl: "shop"
+			linkUrl: "shop/jackets"
 		},
 		{
 			title: "sneakers",
 			imageUrl: "https://i.ibb.co/9gpSd8g/United-boost.jpg",
 			id: 3,
-			linkUrl: "shop"
+			linkUrl: "shop/sneakers"
 		},
 		{
 			title: "womens",
 			imageUrl: "https://i.ibb.co/5vTVCmQ/women-united.jpg",
 			size: "large",
 			id: 4,
-			linkUrl: "shop"
+			linkUrl: "shop/womens"
 		},
 		{
 			title: "mens",
 			imageUrl: david,
 			size: "large",
 			id: 5,
-			linkUrl: "shop"
+			linkUrl: "shop/mens"
 		}
 	]
 };
